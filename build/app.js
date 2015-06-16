f09d978d3f53e25a655dd7af79ef997f7a593eee
RJ Robinson <robertj.robinson@gmail.com>
2015-06-16 17:54:01 -0400

    altered pragma.

--- build/app.js
+++ build/app.js
@@ -1,3 +1,17 @@
 var MessageBox = React.createClass({displayName: "MessageBox",
-
+  render: function() {
+    return (
+    	React.createElement("div", null, 
+      	React.createElement("h1", null, "Hello, World ")
+      )
+    );
+  }
 });
+
+React.render(
+  React.createElement(MessageBox, null),
+  document.getElementById('app'),
+  function () {
+    console.log('after render');
+  }
+)
--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,17 @@
 var MessageBox = React.createClass({
-
+  render: function() {
+    return (
+    	<div>
+      	<h1>Hello, World </h1>
+      </div>
+    );
+  }
 });
+
+React.render(
+  <MessageBox />,
+  document.getElementById('app'),
+  function () {
+    console.log('after render');
+  }
+)
